96b99f1a0e19fa74c2cc1ee0d8734dfd3e77550e
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-02-24 16:24:24 +0000

    testing add feature and should return a certain figure

---
+++ tests/unit/firstTest.php
@@ -0,0 +1,19 @@
+<?php
+
+$phpUnit = 'vendor/bin/phpunit';
+echo $phpUnit;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    
+
+    public function testMultiplicationOfTwoNumber(){
+
+        $a = 5;
+        $b = 4;
+        $c = 5 * 4;
+    $this->assertEquals($c, 20);
+    }    
+}
