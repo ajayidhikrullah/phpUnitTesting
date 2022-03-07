d9682a2142890bda98faa3e2ac302be7d1a439bf
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-03-07 22:22:48 +0000

    checked if an addition is true using the assertEquals function

--- tests/unit/firstTest.php
+++ tests/unit/firstTest.php
@@ -7,13 +7,14 @@ use PHPUnit\Framework\TestCase;
 
 class FirstTest extends TestCase
 {
-    
-
     public function testMultiplicationOfTwoNumber(){
 
         $a = 5;
         $b = 4;
-        $c = 5 * 4;
+        $c = $a * $b;
     $this->assertEquals($c, 20);
     }    
 }
+
+
+// https://www.youtube.com/watch?v=k9ak_rv9X0Y&list=PLfdtiltiRHWGXSggf05W-pJbD47-_d8bJ&index=1
