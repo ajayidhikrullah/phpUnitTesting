0b9b76d942a399faf664777abf8460332c8ccc11
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-03-08 11:59:04 +0000

    refactored the getFirstName and test any input

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,12 +5,14 @@ namespace App\Models;
 class User{
     // 
     public $firstName;
+
     public function setFirstName($firstName){
         $this->firstName = $firstName;
     }
 
     public function getFirstName(){
-    
+        // return 'Dhikrullah';
+       return $this->firstName;
     }
 }
 
--- tests/unit/userModelTest.php
+++ tests/unit/userModelTest.php
@@ -7,11 +7,12 @@ use PHPUnit\Framework\TestCase;
 class UserModelTest extends TestCase{
     //what should user model do
     //get first and last name of user
+
     public function testThatWeCanGetTheFirstName(){
         $user = new \App\Models\User;
-        $fn = $user->setFirstName('Dhikrullah');
-        $checkFn = $user->getFirstName();
-          $this->assertEquals($checkFn, $fn);
+        $user->setFirstName('Dhikrullah');
+        // $user->getFirstName();
+          $this->assertEquals($user->getFirstName(), 'Dhikrullah');
     }
 
     // public function testThatWeCanGetTheLastName()
